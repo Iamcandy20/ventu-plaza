ce8ad8372e0dbbd1be34458f664bfc8ab5ccf268
Dulce Yaquelin Chirinos Sanchez <dulce.chirinos@ladylee.com>
2025-12-16 10:07:36 -0600

    primer commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,15 @@
-import Header from "./components/navbar";
 import Hero from "./components/hero";
 import About from "./components/about";
 import Locations from "./components/locations";
 import Achievements from "./components/cll";
-import Footer from "./components/footer";
 export default function Home() {
   return (
     <>
-      <Header />
       <Hero />
       <About />
       <Locations />
       <Achievements />
-      <Footer />
+
     </>
   );
 }
